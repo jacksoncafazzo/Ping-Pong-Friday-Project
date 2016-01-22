3d8a0cfdebb45623e5ccea8e3247eeea88dfb569
Jackson Cafazzo <doctorjuno@gmail.com>
2016-01-22 14:18:21 -0800

    fix specs to pass for review

--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,13 @@
 describe("getNumberArray", function(userNumber) {
   it("outputs an array from 1 to userNumber with pIng for 3", function() {
-    expect(getNumberArray(3)).to.eql([ 1, 2, 'pIng' ]);
+    expect(getNumberArray(3)).to.eql("<li>1</li><li>2</li><li>pIng</li>");
   });
 
   it("outputs an array from 1 to userNumber with pIng for 3 and poNg for 5", function() {
-    expect(getNumberArray(5)).to.eql([ 1, 2, 'pIng', 4, 'poNg' ]);
+    expect(getNumberArray(5)).to.eql("<li>1</li><li>2</li><li>pIng</li><li>4</li><li>poNg</li>");
   });
 
   it("outputs as array from 1 to userNumber replacing ping, pong and pingpong", function() {
-    expect(getNumberArray(15)).to.eql([1, 2, "pIng", 4, "poNg", "pIng", 7, 8, "pIng", "poNg", 11, "pIng", 13, 14, "pIngPoNg"]);
+    expect(getNumberArray(15)).to.eql("<li>1</li><li>2</li><li>pIng</li><li>4</li><li>poNg</li><li>pIng</li><li><span class=\'glyphicon glyphicon-ice-lolly-tasted\'></span></li><li>8</li><li>pIng</li><li>poNg</li><li><span class=\'glyphicon glyphicon-knight\'></span></li><li>pIng</li><li>13</li><li><span class=\'glyphicon glyphicon-ice-lolly-tasted\'></span></li><li>pIngPoNg</li>");
   });
-  //
-  // it("turns array values into an unordered list items", function() {
-  //   expect(getNumberArray(3)).to.equal('<li>1</li><li>2</li><li>pIng</li>');
-  // });
 });
