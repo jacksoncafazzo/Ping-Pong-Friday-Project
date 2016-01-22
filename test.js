df5d7ec67f9e71294eeda16158c8db51739bc552
brooke hurford and jackson cafazzo <brookebhurford@gmail.com>
2016-01-21 16:32:09 -0800

    edit test.js file

--- test.js
+++ test.js
@@ -1,20 +1,15 @@
 var remainder = function(userInput) {
-  var numberList = [];
   var finalResult = [];
 
   for (var i = 0; i <= userInput; i ++) {
-    // for (var j = 0; j <= userInput; j++) {
-      if (userInput[i] % 3 === 0) {
-          userInput[i] = "pong";
-      } else if {
-        (userInput[i] % 5 === 0);
-        userInput[i] = "ping";
-      } else if {
-        (userInput[i] % 15 === 0);
-        userInput[i] = "pingpong";
-      }
-      finalResult.push(userInput[i]);
+    if (userInput[i] % 3 === 0) {
+        userInput = "pong";
+    } else if (userInput[i] % 5 === 0) {
+      userInput = "ping";
+    } else if (userInput[i] % 15 === 0); {
+      userInput = "pingpong";
     }
+    finalResult = finalResult.appendTo(userInput);
   }
   return finalResult
 }
