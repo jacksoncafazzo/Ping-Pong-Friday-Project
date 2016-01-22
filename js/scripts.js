f74d89b5a9badd529601e9368cb129e0c987d253
Jackson Cafazzo <doctorjuno@gmail.com>
2016-01-21 17:24:44 -0800

    initalize empty files

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,6 @@
-var wordPuzzle = function(sentence) {
-  sentence = sentence.replace(/a|e|i|o|u/gi, "-");
-  return sentence;
-}
 
 
-$(document).ready(function() {
-  $("form#wordPuzzle").submit(function(event) {
+// $(document).ready(function() {
 
-    var userInput = $("input#sentence").val();
-    var puzzleResult = wordPuzzle(userInput);
-    // var puzzleResult = userInput.replace(/a|e|i|o|u/gi, "-");
-
-    $(".puzzleResult").text(puzzleResult);
-    $("#modal").modal('show');
-
-    event.preventDefault();
-  });
-});
+  // });
+// });
--- spec/specs.js
+++
@@ -1,9 +0,0 @@
-describe("wordPuzzle", function(sentence) {
-  it("is true if there are vowels in the sentence", function () {
-    expect(wordPuzzle("Banana")).to.equal(true);
-  });
-
-  it ("it should replace a vowel with a dash", function() {
-    expect(wordPuzzle("Hi friend")).to.equal("H- fr--nd");
-  });
-});
